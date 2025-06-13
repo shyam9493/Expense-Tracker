2766c9c84c9b6e49156346a4ada6cd808a11a51f
shyam9493 <shyamnandapuneedi123@gmail.com>
2025-06-13 23:14:37 +0530

    components

---
+++ src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <>
+    <div className="relative border border-black h-screen overflow-hidden">
+        <div  className='bg-cyan-600 rounded-full absolute z-1 circle'></div>
+
+    </div>
+    </>
+  )
+}
+
+export default Login
