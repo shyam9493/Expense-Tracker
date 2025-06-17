f464276b294f3e070942e816d6c711edfa8a1fb8
shyam9493 <shyamnandapuneedi123@gmail.com>
2025-06-17 23:05:21 +0530

    Base Responsive

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,27 +1,90 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Cards from './Cards'
 
 
+
+
 const Body = () => {
+
+
+
+  const [Income,setIncome]=useState(0);
+  const [Expense,setExpense]=useState(0);
+  const [Transactions,setTransactions] = useState([]);
+
+
+  const handleInput = ()=>{
+    if(!Income && !Expense){
+      alert("Fields should not be empty");
+    }else{
+     setTransactions([...Transactions,{
+        Amount : 100,
+        type : "Income",
+        date : new Date().toLocaleDateString('IND'),
+      }])
+    }    
+  }
+
+
+  useEffect(()=>{
+    console.log(1);
+    
+    console.log(Transactions);  
+  },[Transactions]);
   return (
     <div>
-        <div className='w-[70vw] bg-amber-500 mx-auto ' >
-            <form action="" className='flex items-center p-10 mt-10'>
+        <div className='w-[70vw] bg-amber-500 mx-auto gap-5 flex items-center p-10 mt-10 max-sm:flex-col' >
+          
 
-            <input type="number" placeholder='Enter the amount' className='w-[40%] p-1.5 border'/> 
-            <select className='w-[35%] ml-8 p-1.5 border' required>
+            <input type="number" placeholder='Enter the amount' className='w-[40%] p-1.5 border max-sm:w-[100%] m-0' required/> 
+            <select className='w-[35%]  p-1.5 border max-sm:w-[100%]' required>
                 <option value="" >Select</option>
                 <option>Income</option>
                 <option>Expense</option>
             </select>
-            <input type='submit' className='border p-1 px-8 ml-8' value="Add" />  
+            <input type='submit' className='border p-1 px-8 max-sm:w-[100%]' value="Add"  onClick={handleInput}/>  
 
-            </form>
             
         </div>
-       <Cards type="Income"/>
-       <Cards type="Expense" /> 
-       <Cards type="Savings"/> 
+        <div className='grid grid-cols-3 gap-2 m-2'>
+
+       <Cards type="Income" value={Income}/>
+       <Cards type="Expense" value={Expense}/> 
+       <Cards type="Savings" value={Income - Expense}/> 
+        </div>
+        <div>
+          <table className='border border-2 w-[90%] mx-auto'>
+            <thead>
+            <tr>
+
+              <th>S.No</th>
+              <th>Amount</th>
+              <th>Type</th>
+              <th>Date</th>
+              </tr>
+            </thead>
+            <tbody className='text-center'>
+            
+            
+             {Transactions.length > 0 ? (
+               Transactions.map((Transaction, key) => (
+                 <tr key={key}>
+                   <td>{key + 1}</td>
+                   <td>{Transaction.Amount}</td>
+                   <td>{Transaction.type}</td>
+                   <td>{Transaction.date}</td>
+                 </tr>
+               ))
+             ) : (
+               <tr>
+                 <td colSpan={4} className="text-center">No transactions found</td>
+               </tr>
+             )}
+              
+             
+            </tbody>
+          </table>
+        </div>
     </div>
   )
 }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Cards = (props) => {
   return (
-    <div>Cards : {props.type}</div>
+    <>
+    <div className='container border text-center p-3 bg-amber-300'>
+      <h3>{props.type} : $ {(props.value >= 0)?props.value:0}</h3>
+    </div>
+    </>
   )
 }
 
