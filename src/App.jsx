94c9043646063a7d45d730a237e0942009c44f82
shyam9493 <shyamnandapuneedi123@gmail.com>
2025-06-17 17:04:58 +0530

    SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import Login from './Components/Login'
+import Main from './Components/Main'
+import MainNav from './Components/MainNav'
+
+
+
+
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path='/' element= {<Login />} />
+        <Route path='/main' element= {<MainNav />} />  
+      </Routes>
+      
     </>
   )
-}
+} 
 
 export default App
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './Login.css'
 import { IoIosWallet } from "react-icons/io";
 import { CiLogin } from "react-icons/ci";
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
         <nav className='flex  text-5xl h-screen m-0 pt-5 pl-10'>
           <p className='font-display'>Expense Tracker</p> <p> <IoIosWallet className='' /> </p>
-          <button className="text-blue-500 text-3xl bg-white absolute right-10 z-10 px-5 py-1.5 rounded-full font-display flex items-center gap-2  active:scale-95 transition duration-150 ease-in-out cursor-pointer">
+          
+          <Link to='./main'><button className="text-blue-500 text-3xl bg-white absolute right-10 z-10 px-5 py-1.5 rounded-full font-display flex items-center gap-2  active:scale-95 transition duration-150 ease-in-out cursor-pointer">
             Login <span><CiLogin /></span>
           </button>
+          </Link>
+          
         </nav>   
   )
 }
